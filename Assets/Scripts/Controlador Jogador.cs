be2825ed970a54511f84847c71f750bd7e54577b
LEANDRO CHAEJIIN CASSIMIRO ASSANO <leandro.ccassano@senacsp.edu.br>
2025-02-06 17:48:55 -0300

    Aula 06-02-25

---
+++ Assets/Scripts/Controlador Jogador.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ControladorJogador : MonoBehaviour
+{
+
+    public float taxaMovimentacao;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float altX, altY;
+
+        // Para cima e para baixo mexe com o Y
+        // O && significa "e" em programação, essa parte foi usada para limitar o movimento da luva para cima
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 471)
+            altY = 60 * Time.deltaTime * taxaMovimentacao;
+        else if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > 27)
+            altY = -60 * Time.deltaTime * taxaMovimentacao;
+        else
+            altY = 0;
+
+        // Para os lados mexe com o Y
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > 30 )
+            altX = -60 * Time.deltaTime * taxaMovimentacao;
+        else if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 932)
+            altX = 60 * Time.deltaTime * taxaMovimentacao;
+        else
+            altX = 0;
+
+        // Modificar posição:
+
+        Vector3 novaPos = new Vector3(altX, altY, 0);
+        transform.position += novaPos;
+    }
+}
